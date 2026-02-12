10570a8bf7078ba4e8af6e864e01e15c73e74649
Kusal Kalinga <kusalkalingainfo@gmail.com>
2026-02-12 20:23:49 +0530

    feat: refactor authentication route to use CORS middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import { Hono } from "hono";
 import { auth } from "./lib/auth";
 import { cors } from "hono/cors";
+import { todos } from "./routes/todos.routes";
 
 const app = new Hono();
 
-app.on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw));
+app.use(
+  "/api/auth/*", // or replace with "*" to enable cors for all routes
+  cors({
+    origin: "http://localhost:3001", // replace with your origin
+    allowHeaders: ["Content-Type", "Authorization"],
+    allowMethods: ["POST", "GET", "OPTIONS"],
+    exposeHeaders: ["Content-Length"],
+    maxAge: 600,
+    credentials: true,
+  }),
+);
+
+app
+  .on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw))
+  .route("/api/todos", todos);
 
 export default app;
