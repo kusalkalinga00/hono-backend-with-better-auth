2957e93a7abe1abdb585a83a66cbbb8d687a4f4f
Kusal Kalinga <kusalkalingainfo@gmail.com>
2026-02-12 18:20:28 +0530

    feat: update API authentication routes and improve code formatting

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { auth } from "./lib/auth";
+import { cors } from "hono/cors";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw));
 
-export default app
+export default app;
