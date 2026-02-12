ef9d691ebf8193cac408c2af9fdb22a4f7bd0bbb
Kusal Kalinga <kusalkalingainfo@gmail.com>
2026-02-12 19:41:48 +0530

    feat: implement CRUD operations for Todo model in queries

---
+++ src/queries.ts
@@ -0,0 +1,32 @@
+import { db } from "@/db/db";
+import { Todo } from "@/types";
+import { todos } from "@/db/schema";
+import { desc, eq } from "drizzle-orm";
+
+export const insertTodo = async (todo: Todo) => {
+  const [result] = await db.insert(todos).values(todo).returning();
+  return result;
+};
+
+export const getTodosByUserId = async (userId: string) => {
+  const result = await db
+    .select()
+    .from(todos)
+    .where(eq(todos.userId, userId))
+    .orderBy(desc(todos.updatedAt));
+
+  return result;
+};
+
+export const updateTodo = async (id: string, data: Partial<Todo>) => {
+  const [result] = await db
+    .update(todos)
+    .set(data)
+    .where(eq(todos.id, id))
+    .returning();
+  return result;
+};
+
+export const deleteTodo = async (id: string) => {
+  await db.delete(todos).where(eq(todos.id, id));
+};
